e78ab5f13c8607e2e1932e978209ab6dd6d2e127
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-13 23:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign({}, object, {[key]:value});
+}
+
+function destructivelyIpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
 }
